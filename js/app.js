af2159f2a8324aec555b10fc9fa0152967dadf95
MdAfsarHossain <afsar291722@gmail.com>
2024-09-11 15:50:16 +0600

    Update seats list code added

--- js/app.js
+++ js/app.js
@@ -12,3 +12,21 @@ function updateFieldInnerText(fieldName, value) {
 function getValueFromField(fieldName) {
     return parseFloat(document.getElementById(fieldName).innerText);
 }
+
+
+function updateSeatsList(seatName) {
+    const seatsList = document.getElementById('seats-list');
+    const tr = document.createElement('tr');
+    tr.className = 'w-full text-center';
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    td1.innerText = seatName;
+    td2.innerText = 'Economy';
+    td3.innerText = '550';
+    
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    seatsList.appendChild(tr);
+}
