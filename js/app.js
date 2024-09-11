be981730dfb2152a457a43916e3c207fe91d3272
MdAfsarHossain <afsar291722@gmail.com>
2024-09-11 15:48:28 +0600

    Update field innter text code added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 // Select the seat 
 let totalSelectedSeat = 0;
 let seatResumed = 40;
+
+// Update the filed inner text
+function updateFieldInnerText(fieldName, value) {
+    document.getElementById(fieldName).innerText = value;
+}
